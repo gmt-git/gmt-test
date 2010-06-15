d614f8a92d4d06e1c9685d0685a2217e9e0c27b0
gmt-git <gmt.more@gmail.com>
2010-06-15 17:34:38 +0300

    ticket:1 Змінено ім'я шаблону, попередьно зміненене для перевірки тесту 3

--- djtest/hello/views.py
+++ djtest/hello/views.py
@@ -4,7 +4,7 @@ from djtest.hello.models import Contacts
 def home_page(request):
     obj = Contacts.objects.get(id=1)
     
-    return render_to_response('base.html',
+    return render_to_response('home_page.html',
         {
             "first_name" : obj.first_name,
             "last_name" : obj.last_name,
