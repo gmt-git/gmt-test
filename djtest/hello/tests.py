3b136940cc9163cab75455d4fac5924832c4a020
gmt-git <gmt.more@gmail.com>
2010-06-15 15:45:56 +0300

    ticket:1 Додано перевірку response.context на свпівпадання с initial_data.json

--- djtest/hello/tests.py
+++ djtest/hello/tests.py
@@ -1,3 +1,6 @@
+import os
+import json
+from django.conf import settings
 from django.test import TestCase, client
 
 class HelloTest(TestCase):
@@ -8,3 +11,18 @@ class HelloTest(TestCase):
     def test_homepage_request(self):
         response = self.client.get('/')
         self.failUnlessEqual(response.status_code, 200)
+
+    def test_response_context(self):
+        initial_data_path = os.path.join(settings.PROJ_ROOT, 'hello/fixtures/initial_data.json')
+        initial_data_str = open(initial_data_path).read()
+        data_list = json.loads(initial_data_str)
+
+        first_name = data_list[0]['fields']['first_name']
+        last_name = data_list[0]['fields']['last_name']
+        contact_email = data_list[0]['fields']['contact_email']
+
+        response = self.client.get('/')
+
+        self.failUnlessEqual(response.context['first_name'], first_name)
+        self.failUnlessEqual(response.context['last_name'], last_name)
+        self.failUnlessEqual(response.context['contact_email'], contact_email)
