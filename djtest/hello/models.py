a98934081a7eda863252ead67e8c933a19ae2bcd
gmt-git <gmt.more@gmail.com>
2010-06-15 13:49:45 +0300

    ticket:1 Додано initial_data.json, модель, позбавлення від абсолютних шляхів в settings.py

--- djtest/hello/models.py
+++ djtest/hello/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Contacts(models.Model):
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    contact_email = models.CharField(max_length=100)
