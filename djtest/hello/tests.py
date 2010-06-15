7e91e82fe382767929487e40f418dba2840d48b1
gmt-git <gmt.more@gmail.com>
2010-06-15 14:28:14 +0300

    ticket:1 додана основа для тестів

--- djtest/hello/tests.py
+++ djtest/hello/tests.py
@@ -1,23 +1,10 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
+from django.test import TestCase, client
 
-Replace these with more appropriate tests for your application.
-"""
+class HelloTest(TestCase):
 
-from django.test import TestCase
+    def setUp(self):
+        self.client = client.Client()
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
-
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
-
->>> 1 + 1 == 2
-True
-"""}
-
+    def test_homepage_request(self):
+        response = self.client.get('/')
+        self.failUnlessEqual(response.status_code, 200)
