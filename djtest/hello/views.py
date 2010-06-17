df384abf8cdbbe3f9f210d1771f54b07ecca320e
gmt-git <gmt.more@gmail.com>
2010-06-17 14:42:35 +0300

    ticket:5 Змінено view з метою обробки метода POST

--- djtest/hello/views.py
+++ djtest/hello/views.py
@@ -20,13 +20,27 @@ def cxpr_test(request):
     return render_to_response('cxpr_test.html', context_instance=RequestContext(request))
 
 def edit_contacts(request):
+    errors = []
+    obj = Contacts.objects.all()[0]
+
     if request.method == 'POST':
-        pass 
-    else:
-        obj = Contacts.objects.all()[0]
-        return render_to_response('edit_contacts.html',
-            {
-                "first_name" : obj.first_name,
-                "last_name" : obj.last_name,
-                "contact_email" : obj.contact_email,
-            })
+        if not request.POST.get('first_name', ''):
+            errors.append("Ім'я обов'язкове поле")
+        if not request.POST.get('last_name', ''):
+            errors.append("Фамілія обов'язкове поле")
+        if request.POST.get('contact_email') and '@' not in request.POST['contact_email']:
+            errors.append("Недійсна email адреса")
+        if not errors:
+            obj.first_name = request.POST.['first_name']
+            obj.last_name = request.POST.['last_name']
+            obj.contact_email = request.POST.['contact_email']
+            obj.save()
+            return HttpResponseRedirect('/')
+
+    return render_to_response('edit_contacts.html',
+        {
+            "first_name": obj.first_name,
+            "last_name": obj.last_name,
+            "contact_email": obj.contact_email,
+            "errors": errors
+        })
