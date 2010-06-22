e693d9995b0d7c6bf79d66c27579d67c68c5321e
gmt-git <gmt.more@gmail.com>
2010-06-22 19:35:02 +0300

    ticket:7 Додано тест, ще не завершений, виникли питання з підключенням match_parse

--- djtest/hello/test_tddspry.py
+++ djtest/hello/test_tddspry.py
@@ -22,3 +22,15 @@ class TestCalendar(HttpTestCase):
         for csslinks_for_media in CalendarWidget.Media.css.values():
             for csslink in csslinks_for_media:
                 self.go200(csslink)
+
+    def test_form_order(self):
+        self.extend_with('match_parse')
+        self.go('/accounts/logout/')
+        self.go('/edit_contacts_form/')
+        self.url('/accounts/login/\?next=/edit_contacts_form/')
+        self.fv(1, 'username', 'admin')
+        self.fv(1, 'password', 'admin')
+        self.submit()
+        self.go('/edit_contacts_form/')
+        #mlist = self.findall('name=.(birth_date|first_name).')
+        self.ok_(False)
