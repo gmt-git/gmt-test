7fd11090c39344c1fdfbf5fe83546205a0af84c1
gmt-git <gmt.more@gmail.com>
2010-06-23 14:55:38 +0300

    ticket:8 Додано клас який буде рендеріть тег

--- djtest/hello/templatetags/edit_list_lib.py
+++ djtest/hello/templatetags/edit_list_lib.py
@@ -2,6 +2,9 @@ from django import template
 
 register = template.Library()
 
+class EditListNode(template.Node):
+    pass
+
 @register.tag(name='edit_list')
 def do_edit_list(parser, token):
-    pass
+    return EditListNode()
