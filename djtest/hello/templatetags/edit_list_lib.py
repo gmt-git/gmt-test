6f5d4ec29ba798f3332ccd40e67263f2a02c111e
gmt-git <gmt.more@gmail.com>
2010-06-25 14:01:10 +0300

    ticket:8 Додано реалізацію рендерінгу у вигляді таблиці

--- djtest/hello/templatetags/edit_list_lib.py
+++ djtest/hello/templatetags/edit_list_lib.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+
 from django import template
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.admin.models import LogEntry
@@ -24,11 +27,22 @@ class EditListNode(template.Node):
             obj_id = None
             ct_id = None
 
+        qs = LogEntry.objects.filter(content_type=ct_id, object_id=obj_id)
         if self.test:
-            self.result = ','.join( \
-                LogEntry.objects.filter(content_type=ct_id, object_id=obj_id). \
-                values_list('change_message', flat=True)
-            )
+            self.result = ','.join(qs.values_list('change_message', flat=True))
+        else:
+            if qs.count() > 0:
+                self.result += u'<table><caption>Зміни %s</caption><tr>' % qs[0].object_repr
+                self.result += u'<th>%s</th><th>%s</th><th>%s</th><th>%s</th>' % \
+                    ('action_time', 'user', 'content_type', 'object_repr')
+                self.result += u'<th>%s</th><th>%s</th></tr>' % ('action_flag', 'change_message')
+
+                for r in qs:
+                    self.result += u'<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td>' % \
+                        (r.action_time, r.user, r.content_type, r.object_repr)
+                    self.result += u'<td>%s</td><td>%s</td></tr>' % (r.action_flag, r.change_message)
+
+                self.result += u'</table>'
 
         return self.result
 
