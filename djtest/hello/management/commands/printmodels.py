4a4dee9e3eb4ef1f9042d9409b55a6f003e92324
gmt-git <gmt.more@gmail.com>
2010-06-28 15:53:35 +0300

    ticket:9 Додано тест на статічних данних

--- djtest/hello/management/commands/printmodels.py
+++ djtest/hello/management/commands/printmodels.py
@@ -10,12 +10,12 @@ class Command(NoArgsCommand):
     help = 'Команда друкує всі моделі проекту'
 
     def handle_noargs(self, *args, **options):
-        sys.stdout.write('Test Error')
+        sys.stdout.write(get_models_and_count())
 
 def get_models_and_count():
-    result = {}
+    result = ''
     for ct in ContentType.objects.order_by('id'):
-        result[ct.app_label+'.'+ct.model] = ct.model_class().objects.count()
+        result += ct.app_label + '.' + ct.model + ' count=' + str(ct.model_class().objects.count()) + '\n'
     return result
 
 def handle_test():
@@ -23,6 +23,14 @@ def handle_test():
     >>> from hello.management.commands.printmodels import Command, get_models_and_count
     >>> c = Command()
     >>> c.handle_noargs()
-    test OK
+    admin.logentry count=0
+    auth.permission count=30
+    auth.group count=0
+    auth.user count=1
+    auth.message count=0
+    contenttypes.contenttype count=10
+    sessions.session count=0
+    sites.site count=1
+    hello.contacts count=1
+    hello.httpreqs count=0
     """
-
