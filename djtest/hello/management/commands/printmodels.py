46faa6fe00ab0241c5651827d4278e5faa15bbd8
gmt-git <gmt.more@gmail.com>
2010-06-28 16:20:53 +0300

    ticket:9 Тест змінено з використанням еліпсіса

--- djtest/hello/management/commands/printmodels.py
+++ djtest/hello/management/commands/printmodels.py
@@ -7,15 +7,16 @@ from django.contrib.contenttypes.models import ContentType
 from django.core.management.base import NoArgsCommand, CommandError
 
 class Command(NoArgsCommand):
-    help = 'Команда друкує всі моделі проекту'
+    help = 'Print project models and object count'
 
     def handle_noargs(self, *args, **options):
         sys.stdout.write(get_models_and_count())
 
 def get_models_and_count():
-    result = ''
+    result = 'Project models:\n\n'
     for ct in ContentType.objects.order_by('id'):
         result += ct.app_label + '.' + ct.model + ' count=' + str(ct.model_class().objects.count()) + '\n'
+    result += '\nTotal models count: %d' % ContentType.objects.all().count()
     return result
 
 def handle_test():
@@ -23,14 +24,10 @@ def handle_test():
     >>> from hello.management.commands.printmodels import Command, get_models_and_count
     >>> c = Command()
     >>> c.handle_noargs()
-    admin.logentry count=0
-    auth.permission count=30
-    auth.group count=0
-    auth.user count=1
-    auth.message count=0
-    contenttypes.contenttype count=10
-    sessions.session count=0
-    sites.site count=1
+    Project models:
+    ...
     hello.contacts count=1
     hello.httpreqs count=0
+    ...
+    Total models count...
     """
