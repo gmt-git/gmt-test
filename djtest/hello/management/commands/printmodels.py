c22547c83cee481416ebd3776572e4032e15e0bf
gmt-git <gmt.more@gmail.com>
2010-06-28 15:31:29 +0300

    ticket:9 Додано функцію що створює словник з моделей та кількості об'єктів

--- djtest/hello/management/commands/printmodels.py
+++ djtest/hello/management/commands/printmodels.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*- 
 
 import sys
+
+from django.contrib.contenttypes.models import ContentType
 from django.core.management.base import NoArgsCommand, CommandError
 
 class Command(NoArgsCommand):
@@ -10,9 +12,15 @@ class Command(NoArgsCommand):
     def handle_noargs(self, *args, **options):
         sys.stdout.write('Test Error')
 
+def get_models_and_count():
+    result = {}
+    for ct in ContentType.objects.order_by('id'):
+        result[ct.app_label+'.'+ct.model] = ct.model_class().objects.count()
+    return result
+
 def handle_test():
     """
-    >>> from hello.management.commands.printmodels import Command
+    >>> from hello.management.commands.printmodels import Command, get_models_and_count
     >>> c = Command()
     >>> c.handle_noargs()
     test OK
