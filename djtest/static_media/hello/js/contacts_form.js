c1394237d2ef3f34c306194c61b4136d2c642bc4
gmt-git <gmt.more@gmail.com>
2010-06-29 23:08:36 +0300

    ticket:11 Додано обробку подій, виправлено поведінку календаря

--- djtest/static_media/hello/js/contacts_form.js
+++ djtest/static_media/hello/js/contacts_form.js
@@ -1,8 +1,17 @@
 $(function() {
     var options = {
         target:        '#update_target',
+        beforeSubmit:  on_before_submit,
+        success:       on_success
     };
 
     $('FORM').ajaxForm(options);
 });
 
+function on_before_submit(formData, jqForm, options) {
+    return true;
+}
+
+function on_success(responseText, statusText, xhr, $form)  {
+    DateTimeShortcuts.init();
+}
