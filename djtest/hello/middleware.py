448615b2916a539e15007baae54de7f88975a6f1
gmt <gmt.more@gmail.com>
2011-05-23 03:49:41 +0300

    pep8 amend

--- djtest/hello/middleware.py
+++ djtest/hello/middleware.py
@@ -8,7 +8,7 @@ from djtest.hello.models import HttpReqs
 class HttpReqsSave():
 
     def process_request(self, request):
-        req = HttpReqs(full_path = request.get_full_path())
+        req = HttpReqs(full_path=request.get_full_path())
         req.date = datetime.now()
         req.method = request.method
         req.meta = "\n".join(["%s=%s" % (k, v) for k, v in request.META.items()])
